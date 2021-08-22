be4ba2dd30831ecd673495b7d7ae4ae3f2c8cbf2
Jorge Torres <jorge.albertorres@gmail.com>
2021-08-22 00:40:12 -0500

    Add files via upload

---
+++ 01 Conceptos basicos/E1_11_area_numero_indeterminado_while.c
@@ -0,0 +1,41 @@
+/* programa para calcular el área de un circulo */
+/* Compilado en Windows 10  y gcc-10.2.0 */
+/* Cap1 Conceptos Básicos , Pagina 18
+/* Programacion en C, SCHAUM Byron Gottfried */
+/* Programa para calcular áreas de varios círculos, usando un bucle for; no se especifica el numero de circulos */
+
+#include <stdio.h>
+#define PI 3.14159
+float procesar(float radio); /* Prototipo de Funcion */
+
+/*main()*/
+int main(int argc, char const *argv[])
+
+{
+    float radio, area; /* Declaración de variables */
+     
+    printf("Para PARAR, introducir 0 en el valor del radio\n");
+    printf("\nRadio = ?");
+    scanf("%f", &radio);
+    
+    while (radio != 0) {
+        
+        if (radio < 0)
+        area = 0;
+        else
+        area = procesar(radio);
+
+    printf("Area = %f\n", area);
+    printf("\nRadio =?");
+    scanf("%f", &radio);
+    }
+   
+}
+
+float procesar(float r)  /* Definicion de funcion  */
+
+{
+    float a;
+    a = PI * r * r;
+    return(a);
+}
