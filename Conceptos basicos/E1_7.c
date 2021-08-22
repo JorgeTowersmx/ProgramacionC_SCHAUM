1ee58b8043d864a0988b8391d28a3d8c4400daa8
Jorge Torres <jorge.albertorres@gmail.com>
2021-08-22 00:04:19 -0500

    Add files via upload

---
+++ Conceptos basicos/E1_7.c
@@ -0,0 +1,18 @@
+/* programa para calcular el área de un circulo */
+/* Compilado en Windows 10  y gcc-10.2.0 */
+/* Cap1 Conceptos Básicos , Pagina 12 
+/* Programacion en C, SCHAUM Byron Gottfried */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+
+{
+     float radio, area;
+
+    printf("Radio = ? " );
+    scanf("%f", &radio );
+    area = 3.14159 * radio * radio;
+    printf("Area = %f", area);
+    
+}
---
+++ Conceptos basicos/E1_9_area_varios_circulos.c
@@ -0,0 +1,41 @@
+/* programa para calcular el área de un circulo */
+/* Compilado en Windows 10  y gcc-10.2.0 */
+/* Cap1 Conceptos Básicos , Pagina 12 
+/* Programacion en C, SCHAUM Byron Gottfried */
+/* Programa para calcular áreas de varios círculos usando un bucle for */
+#include <stdio.h>
+#define PI 3.14159
+float procesar(float radio); /* Prototipo de Funcion */
+
+/*main()*/
+int main(int argc, char const *argv[])
+
+{
+    float radio, area; /* Declaración de variables */
+    int cont, n;       /* Declaración de variables */ 
+
+    printf("Numero de círculos?");
+    scanf("%d", &n);
+    
+    for (cont = 1; cont <=n; ++cont){
+        printf("\nCirculo n° %d: Radio = ? ", cont);
+        scanf("%f", &radio );
+
+    if (radio < 0)
+        area = 0;
+    else
+        area = procesar(radio);
+
+    printf("Area = %f", area);
+    }
+   
+    
+}
+
+float procesar(float r)  /* Definicion de funcion  */
+
+{
+    float a;
+    a = PI * r * r;
+    return(a);
+}
